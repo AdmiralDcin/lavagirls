63d0d4f0abefd79c990e540bf72160088e11bf8b
AdmiralDcin <66283896+AdmiralDcin@users.noreply.github.com>
2024-04-09 23:56:51 +0700

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-c0c66d43-9407-4106-9de8-0d1580419412/"
 
 OUTPUT_DIR="logs"
 
